410bde7a8afe99f26ed90aa34975c340f6cb3b0f
Maciej <sppmacd@pm.me>
2022-05-02 13:18:47 +0200

    Make browser-ui-loader actually load the script from github

--- browser-ui-loader.js
+++ browser-ui-loader.js
@@ -1,1 +1,12 @@
-document.body.innerHTML += `<script src="https://github.com/sppmacd/canvas-utils/blob/master/browser-ui.js"></script>`;
+(async () => {
+    // cleanup old script elements
+    for (const e of document.body.getElementsByClassName("x-canvas-utils-loader"))
+        e.remove();
+
+    // fetch the script from github and run it
+    let s = document.createElement("script");
+    s.text = await (await fetch("https://raw.githubusercontent.com/sppmacd/canvas-utils/master/browser-ui.js")).text();
+    s.type = "text/javascript";
+    s.class = "x-canvas-utils-loader";
+    document.body.appendChild(s);
+})()
